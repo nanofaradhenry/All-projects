398792065284e42d96a0a717cd5ee5692bd72fd0
Mona Lisa <ischavez1111@gmail.com>
2022-04-18 06:43:53 -0500

    added js file to html

--- ProfessionalWebsite/script.js
+++ ProfessionalWebsite/script.js
@@ -1,1 +1,16 @@
-alert("Welcome to @nanofaradhenry's webpage")
+alert("Welcome to @nanofaradhenry's webpage");
+// test and introductory code
+console.log("Welcome to the program");
+console.log("calculating" ,23+4);
+
+//test and introductory code 
+
+
+
+let continent = (true);
+console.log(typeof(continent))
+
+let man=('person');
+console.log(typeof(man));
+
+
